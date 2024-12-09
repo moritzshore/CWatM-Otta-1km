abf036b8f507b28a6b9621720fb45ce70c8a145f
Moritz Shore <moritzshore@gmail.com>
2024-12-09 12:34:33 -0500

    edit batch file

---
+++ run cwatm.bat
@@ -0,0 +1,2 @@
+C:\Users\mosh\AppData\Local\miniconda3\envs\jessica\python.exe C:\Users\mosh\Documents\GIT\CWatM\run_cwatm.py settings/settings_norway_calibration_June_v2-Best_5yr_crops.ini -l
+pause
